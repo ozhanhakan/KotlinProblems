daad5f471e4df405405f4e92017108f1040bdc98
Hakan Ozhan <ozhanhakan@gmail.com>
2022-06-20 19:26:16 +0300

    commit not predicate

--- Topics/Lambda expressions/ Not  predicate/src/Main.kt
+++ Topics/Lambda expressions/ Not  predicate/src/Main.kt
@@ -1,1 +1,8 @@
-val notPredicate: (Char) -> Boolean = ::!originalPredicate
+val notPredicate: (Char) -> Boolean = { !originalPredicate(it) }
+/*
+https://stackoverflow.com/questions/63225572/lambda-expressions-not-predicate
+    val notPredicate: (Char) -> Boolean = { char ->
+    val booleanValue = originalPredicate(char)
+    return@notPredicate !booleanValue
+    // val notPredicate: (Char) -> Boolean = { !originalPredicate(it) }
+}*/
