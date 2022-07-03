4ecb583785c383c83db3e1d5b605385c356bb72b
Hakan Ozhan <ozhanhakan@gmail.com>
2022-07-03 11:30:28 +0300

    Problem of the day / enum property

---
+++ Topics/Enum/Danger level/src/Main.kt
@@ -0,0 +1,19 @@
+const val HIGH_LEVEL = 3
+const val MEDIUM_LEVEL = 2
+const val LOW_LEVEL = 1
+
+enum class DangerLevel(val num: Int) {
+    HIGH(HIGH_LEVEL),
+    MEDIUM(MEDIUM_LEVEL),
+    LOW(LOW_LEVEL);
+
+    fun getLevel(): Int {
+        return this.num
+    }
+}
+
+/*fun main() {
+    val high = DangerLevel.HIGH
+    val medium = DangerLevel.MEDIUM
+    println(high.getLevel() > medium.getLevel())
+}*/
